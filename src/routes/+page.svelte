a50e38cf09d6f92972a1e94f4ac80cbb0dfba88b
Abhinav <63947769+abhinavdark@users.noreply.github.com>
2024-08-05 22:05:57 +0530

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,16 +7,12 @@
 
 <svelte:head>
 	<title>Portfolio | NotYasho</title>
-	<meta name="title" content="Portfolio | NotYasho" />
+	<meta name="title" content="Blogs | Vegamedia" />
 	<meta
 		name="description"
-		content="Hi! This is my portfolio site as a (emerging) software developer :P. Explore my projects, experience, and blog posts to learn more."
+		content="Vegamedia is a comprehensive digital marketing blog that focuses on empowering businesses to succeed in the online landscape. It offers a variety of insights and strategies related to digital marketing, including SEO, content marketing, social media management, and paid advertising"
 	/>
-	<meta
-		name="image"
-		content="https://raw.githubusercontent.com/NotYasho/notyasho.netlify.app/master/static/img/thumbnails/portfolio.png"
-		class="next-head"
-	/>
+	
 
 	<!-- Open Graph / Facebook -->
 	<meta property="og:type" content="website" />
@@ -24,9 +20,9 @@
 	<meta property="og:title" content="NotYasho - Portfolio" />
 	<meta
 		property="og:description"
-		content="Hi! This is my portfolio site as a (emerging) software developer :P. Explore my projects, experience, and blog posts to learn more."
+		content="Vegamedia is a comprehensive digital marketing blog that focuses on empowering businesses to succeed in the online landscape. It offers a variety of insights and strategies related to digital marketing, including SEO, content marketing, social media management, and paid advertising"
 	/>
-	<meta property="og:image" content="https://raw.githubusercontent.com/NotYasho/notyasho.netlify.app/master/static/img/thumbnails/portfolio.png" />
+	
 
 	<!-- Twitter -->
 	<meta property="twitter:card" content="summary_large_image" />
@@ -34,12 +30,9 @@
 	<meta property="twitter:title" content="NotYasho - Portfolio" />
 	<meta
 		property="twitter:description"
-		content="Hi! This is my portfolio site as a (emerging) software developer :P. Explore my projects, experience, and blog posts to learn more."
+		content="Vegamedia is a comprehensive digital marketing blog that focuses on empowering businesses to succeed in the online landscape. It offers a variety of insights and strategies related to digital marketing, including SEO, content marketing, social media management, and paid advertising"
 	/>
-	<meta
-		property="twitter:image"
-		content="https://raw.githubusercontent.com/NotYasho/notyasho.netlify.app/master/static/img/thumbnails/portfolio.png"
-	/>
+	
 </svelte:head>
 
 <p>placeholder</p>
