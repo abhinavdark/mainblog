e7f089f3abbdec27c6f20e2a5f939b28f07131b0
Abhinav <63947769+abhinavdark@users.noreply.github.com>
2024-08-11 13:35:24 +0530

    Update sanity.cli.js

--- studio/sanity.cli.js
+++ studio/sanity.cli.js
@@ -1,8 +1,8 @@
 import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
-  api: {
-    projectId: 'u62jjaxi',
-    dataset: 'production'
-  }
+  project: {
+    basePath: '/studio'
+  },
+  // ...config continued
 })
